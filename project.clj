9eb712772ff887c03b3d59ad9cc3bba90bb6a31c
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2016-07-23 00:15:11 +0200

    Heroku setup
    I actually think the project changes are now outdated, but they
    shouldn't hurt.
    
    Notice how the port is specified in the Profile. This is because Heroku
    dictates this in the environment.

--- project.clj
+++ project.clj
@@ -7,6 +7,8 @@
                  [javax.servlet/servlet-api "2.5"]
                  [ring/ring "1.5.0"]
                  [compojure "1.5.1"]]
+  :min-lein-version "2.0.0"
+  :uberjar "webdev.jar"
   :main webdev.core
   :profiles {:dev
              {:main webdev.core/-dev-main}})
