ae8aa1abf7536e95199a3fd36afd44937db3fa47
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2016-07-23 00:02:01 +0200

    Initial commit
    Very simple Ring handler implemented. Code reloading in dev mode
    implemented.

---
+++ src/webdev/core.clj
@@ -0,0 +1,18 @@
+(ns webdev.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]))
+
+(defn greet [req]
+  (if (= "/" (:uri req))
+    {:status 200
+     :body "Hello world2"
+     :headers {}}
+    {:status 404
+     :body "Page not found"
+     :headers {}}))
+
+(defn -main [port]
+  (jetty/run-jetty greet { :port (Integer. port)}))
+
+(defn -dev-main [port]
+  (jetty/run-jetty (wrap-reload #'greet) {:port (Integer. port)}))
