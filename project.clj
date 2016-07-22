289e7885e6bf10084b9eda028ae771d1f9c323cf
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2016-07-23 00:21:53 +0200

    Fix uberjar name config

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [ring/ring "1.5.0"]
                  [compojure "1.5.1"]]
   :min-lein-version "2.0.0"
-  :uberjar "webdev.jar"
+  :uberjar-name "webdev.jar"
   :main webdev.core
   :profiles {:dev
              {:main webdev.core/-dev-main}})
