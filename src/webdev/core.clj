03dd352b9ecbc9780ae1daff6eea9ce464b308b0
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2016-07-23 22:04:22 +0200

    Ex 7: Handler to print the request
    This version uses pprint wrapped with "with-out-str".

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -2,7 +2,8 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [not-found]]))
+            [compojure.route :refer [not-found]]
+            [clojure.pprint :refer [pprint]]))
 
 (defn greet [req]
   {:status 200
@@ -19,10 +20,16 @@
    :body "Created by Jacob Tjoernholm while learning about web development in Clojure using the Purely Functional lessons by Eric Normand (purelyfunctional.tv)"
    :headers {}})
 
+(defn request [req]
+  {:status 200
+   :body (with-out-str (pprint req))
+   :header {}})
+
 (defroutes app
   (GET "/" [] greet)
-  (GET "/goodbye" [] goodbye)
   (GET "/about" [] about)
+  (GET "/goodbye" [] goodbye)
+  (GET "/request" [] request)
   (not-found "Page not found"))
 
 (defn -main [port]
