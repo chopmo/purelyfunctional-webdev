e9ec4bf2622c92580e2bb1201d36a10ce2abca65
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2016-07-24 13:57:55 +0200

    List of items in Hiccup

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [ring/ring "1.5.0"]
                  [compojure "1.5.1"]
                  [org.clojure/java.jdbc "0.6.2-alpha2"]
-                 [postgresql/postgresql "9.1-901-1.jdbc4"]]
+                 [postgresql/postgresql "9.1-901-1.jdbc4"]
+                 [hiccup "1.0.5"]]
   :min-lein-version "2.0.0"
   :uberjar-name "webdev.jar"
   :main webdev.core
